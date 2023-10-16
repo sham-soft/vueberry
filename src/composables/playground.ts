3bd73c3a116c59501d6bd2ffc8c4f70b3d2df04c
sh.tepsurkaev <shamil.tepsurkaev@simbirsoft.com>
2023-10-17 00:34:12 +0400

    new updates

--- src/composables/playground.ts
+++
@@ -1,9 +0,0 @@
-import type { TypeParameterValue, TypeData } from '@/components/common/VPlayground/types';
-
-export function useParameter() {
-    function setValue(playgroundValues: TypeParameterValue, data: TypeData): void {
-        playgroundValues[data.key] = data.value;
-    }
-    
-    return { setValue };
-}
